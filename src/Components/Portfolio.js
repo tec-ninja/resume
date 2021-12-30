714b05d77ec8ad4ec84dea3a54b7d301df524cf8
tec-ninja <devstar1112@gmail.com>
2021-12-30 10:52:53 +0800

    update portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Portfolio extends Component {
-  render() {
+function Portfolio(props) {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.images[0];
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage}  style={{height: 120}} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-      })
-    }
+  const projects = props.data.projects.map(project => {
+    let projectImage = 'images/portfolio/'+project.images[0];
+    return <div key={project.title} className="columns portfolio-item">
+        <div className="item-wrap">
+        <a href={project.url} title={project.title}>
+            <img alt={project.title} src={projectImage}  style={{height: 120}} />
+            <div className="overlay">
+              <div className="portfolio-item-meta">
+              <h5>{project.title}</h5>
+                  <p>{project.category}</p>
+              </div>
+            </div>
+          <div className="link-icon"><i className="fa fa-link"></i></div>
+        </a>
+      </div>
+    </div>
+  })
 
     return (
       <section id="works">
@@ -39,7 +36,6 @@ class Portfolio extends Component {
       </div>
    </section>
     );
-  }
 }
 
 export default Portfolio;
